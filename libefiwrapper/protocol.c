9528865ea7f3844555d32884688201ca33ce8001
Ni, Erchang <erchang.ni@intel.com>
2024-07-08 08:54:40 +0000

    Fix Resource Leak and Uninitialized variable
    Tracked-On: OAM-121841
    Signed-off-by: Ni, Erchang <erchang.ni@intel.com>

--- libefiwrapper/protocol.c
+++ libefiwrapper/protocol.c
@@ -241,8 +241,12 @@ locate_device_path(EFI_GUID *Protocol,
 		return EFI_INVALID_PARAMETER;
 
 	ret = locate_handle_buffer(ByProtocol, Protocol, NULL, &nb_handle, &handles);
-	if (EFI_ERROR(ret) || nb_handle == 0)
+	if (EFI_ERROR(ret) || nb_handle == 0) {
+		if (handles) {
+			free(handles);
+		}
 		return EFI_NOT_FOUND;
+	}
 
 	for (i = 0; i < nb_handle; i++) {
 		ret = handle_protocol(handles[i], &dp_guid, (VOID **)&path);
