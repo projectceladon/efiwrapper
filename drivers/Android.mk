6a33490da24e1e3109eafafc35b1f165a4b39043
Haoyu Tang <haoyu.tang@intel.com>
2024-08-20 13:58:27 +0800

    efiwrapper: Add dedicated efiwrapper libraries for CrashDump
    Test Done:
    1. android boot
    2. crashdump function works
    
    ori-Tracked-On: OAM-126205
    Tracked-On: OAM-127436
    Signed-off-by: Haoyu Tang <haoyu.tang@intel.com>
    Signed-off-by: Bo He <bo.he@intel.com>

--- drivers/Android.mk
+++ drivers/Android.mk
@@ -40,3 +40,42 @@ LOCAL_CFLAGS := $(EFIWRAPPER_CFLAGS)
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../include/hardware
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../external/coreboot/payloads/libpayload/include
 include $(BUILD_IAFW_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libefiwrapper_drivers-cd-$(TARGET_BUILD_VARIANT)
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+LOCAL_STATIC_LIBRARIES := \
+	libpayload \
+	libefiwrapper-cd-$(TARGET_BUILD_VARIANT) \
+	libgnuefi \
+	libefi
+LOCAL_SRC_FILES := \
+	$(foreach drv, $(LIBEFIWRAPPER_DRIVERS_CD), \
+		$(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/$(drv)/*.c)))
+GEN := $(local-generated-sources-dir)/drivers.c
+ifeq ($(LIBEFIWRAPPER_DRIVERS_CD),)
+$(GEN):
+	$(hide) mkdir -p $(dir $@)
+	$(hide)	echo "/* Do not modify this auto-generated file. */" > $@
+	$(hide) echo "#error \"no driver selected.  Cf. LIBEFIWRAPPER_DRIVERS_CD Makefile variable.\"" >> $@
+else
+$(GEN):
+	$(hide) mkdir -p $(dir $@)
+	$(hide) echo "/* Do not modify this auto-generated file. */" > $@
+	$(hide) echo "#include \"ewdrv.h\"" >> $@
+	$(hide) $(foreach drv, $(LIBEFIWRAPPER_DRIVERS_CD), echo "#include" \"$(drv)/$(drv).h\" >> $@;)
+	$(hide) echo "" >> $@
+	$(hide) echo "static ewdrv_t *drivers[] = {" >> $@
+	$(hide) $(foreach drv, $(LIBEFIWRAPPER_DRIVERS_CD), echo "&"$(drv)_drv, >> $@;)
+	$(hide) echo  "NULL" >> $@
+	$(hide) echo "};" >> $@
+	$(hide) echo "" >> $@
+	$(hide) echo "ewdrv_t **ew_drivers = drivers;" >> $@
+endif
+LOCAL_GENERATED_SOURCES := $(GEN)
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
+LOCAL_CFLAGS := $(EFIWRAPPER_CFLAGS)
+LOCAL_CFLAGS += -D__CRASH_DUMP
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../include/hardware
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../external/coreboot/payloads/libpayload/include
+include $(BUILD_IAFW_STATIC_LIBRARY)
